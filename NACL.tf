c52e5bb33eb025b7ab1a8df17d456282c8685035
Austin Cottrell <68828104+quiik@users.noreply.github.com>
2021-01-31 22:52:24 -0800

    Update NACL.tf

--- NACL.tf
+++ NACL.tf
@@ -14,6 +14,18 @@ resource "aws_network_acl" "secure_private_subnet" {
     }
   }
 
+  dynamic "ingress" {
+    for_each = var.nacl_server_ingress_ports
+    content {
+      from_port  = ingress.value["port"]
+      to_port    = ingress.value["port"]
+      protocol   = ingress.value["type"]
+      action     = "allow"
+      cidr_block = "0.0.0.0/0"
+      rule_no    = ingress.value["rule"]
+    }
+  }
+
   egress {
     protocol   = "-1"
     rule_no    = 100
